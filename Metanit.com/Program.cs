88cfcb1ede531bd12b2becc1392e25816ee9cdeb
Sergey <sergnikolaev89@yandex.ru>
2022-11-24 23:16:16 +0300

    Удалил папку Example_01_Pasword_login

---
+++ Metanit.com/Program.cs
@@ -0,0 +1,16 @@
+﻿// Переменные и константы.
+/*
+string name = "Tom"; // определяем переменную и инициализируем её
+Console.WriteLine(name); // выводит Tom
+
+name = "Bob"; // меняем значение переменной
+Console.WriteLine(name); // выводит Bob
+*/
+
+/*
+const string NAME = "Tom"; // определяем константу
+// const string NAME; // ! ошибка, константа NAME не инициализирована 
+// NAME = "Bob"; // ! ошибка, у константы нельзя изменить значение
+Console.WriteLine(NAME);
+*/
+// Литералы.
